d39ec57df5cce9ec8a1da20d110ee8dfe8c0731d
Dicksen Veloopillay <dicksen.veloopillay@proximitybbdo.fr>
2020-01-15 16:02:06 +0400

    Updates

---
+++ precache-manifest.ac12a9d3465e8dc8f6707c9991a25cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64fad7978aec935bea9478edce50df5",
+    "url": "/grocery-app/index.html"
+  },
+  {
+    "revision": "ef82374be83814f8e285",
+    "url": "/grocery-app/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "1079eacf8834c076b07b",
+    "url": "/grocery-app/static/css/main.f9c2d115.chunk.css"
+  },
+  {
+    "revision": "ef82374be83814f8e285",
+    "url": "/grocery-app/static/js/2.02943203.chunk.js"
+  },
+  {
+    "revision": "1079eacf8834c076b07b",
+    "url": "/grocery-app/static/js/main.19371a1a.chunk.js"
+  },
+  {
+    "revision": "b8d9b953abf157c4129d",
+    "url": "/grocery-app/static/js/runtime~main.b23f09fe.js"
+  }
+]);
